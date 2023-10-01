92a49ad07dc34a57d48cd46c22d5ea3045a197bc
Rishabh Chopade <rishabhchopade15@gmail.com>
2023-10-01 23:00:00 +0530

    Installed Npm

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,16 @@
-console.log("This is my backend server");
+// Importing required modules
+const express = require("express");
+
+// Creating an Express application
+const app = express();
+
+// Define a route
+app.get("/", (req, res) => {
+  res.send("Hello, this is your Express server!");
+});
+
+// Set up the server to listen on a specific port (e.g., 3000)
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
